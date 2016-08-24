bda5491b3ef05980da750f7525a4f27c27e883d0
aidan <aidan.dodds@googlemail.com>
2016-08-24 23:21:39 +0100

    refactor spatial code

--- framework/spatial.cpp
+++ framework/spatial.cpp
@@ -1,7 +1,8 @@
 #include <assert.h>
 #include "spatial.h"
 
-namespace {
+namespace
+{
     float clamp(
         const float min,
         const float in,
@@ -11,10 +12,29 @@ namespace {
         const float t1 = (t0 > max) ? max : t0;
         return t1;
     }
+
+    bool overlap(
+            const body_t * obj,
+            float x0,
+            float y0,
+            float x1,
+            float y1)
+    {
+        assert(obj);
+        const vec2f_t p = obj->pos();
+        const float radius = obj->radius();
+        const float cx = clamp( x0, p.x, x1 );
+        const float cy = clamp( y0, p.y, y1 );
+        const float dx = p.x - cx;
+        const float dy = p.y - cy;
+        const float ds = dx*dx + dy*dy;
+        return ds < (radius * radius);
+    }
 } // namespace {}
 
 void body_pair_set_t::insert(body_t * a, body_t * b)
 {
+    assert(a && b);
     base_.insert(pair_t(a, b));
 }
 
@@ -25,10 +45,13 @@ void body_pair_set_t::clear()
 
 void body_set_t::insert(body_t * a)
 {
+    assert(a);
     base_.insert(a);
 }
 
-bool body_set_t::operator[] (body_t * a) const {
+bool body_set_t::operator[] (body_t * a) const
+{
+    assert(a);
     return base_.find(a) != base_.cend();
 }
 
@@ -39,21 +62,23 @@ void body_set_t::clear()
 
 std::list<spatial_t::slot_t> & spatial_t::slot(int32_t x, int32_t y)
 {
-    return hash_[(x+y * (512/width))%hash_.size()];
+    return hash_[(x + y * (512/width))%hash_.size()];
 }
 
-spatial_t::bound_t spatial_t::object_bound(body_t * obj)
+spatial_t::bound_t spatial_t::object_bound(const body_t * obj) const
 {
+    assert(obj);
     return bound_t {
-        int32_t((obj->x-obj->r) / width),
-        int32_t((obj->y-obj->r) / width),
-        int32_t((obj->x+obj->r) / width),
-        int32_t((obj->y+obj->r) / width)
+        int32_t((obj->p.x-obj->r) / width),
+        int32_t((obj->p.y-obj->r) / width),
+        int32_t((obj->p.x+obj->r) / width),
+        int32_t((obj->p.y+obj->r) / width)
     };
 }
 
 void spatial_t::insert(body_t * obj)
 {
+    assert(obj);
     bound_t ob = object_bound(obj);
     for (int32_t y = ob.y0; y<=ob.y1; ++y) {
         for (int32_t x = ob.x0; x<=ob.x1; ++x) {
@@ -62,8 +87,12 @@ void spatial_t::insert(body_t * obj)
     }
 }
 
-void spatial_t::slot_erase(int32_t x, int32_t y, body_t * obj)
+void spatial_t::slot_erase(
+    int32_t x,
+    int32_t y,
+    const body_t * obj)
 {
+    assert(obj);
     auto & list = slot(x, y);
     for (auto itt = list.begin(); itt!=list.end();) {
         if (itt->obj==obj && itt->x==x && itt->y==y) {
@@ -75,13 +104,16 @@ void spatial_t::slot_erase(int32_t x, int32_t y, body_t * obj)
     }
 }
 
-void spatial_t::slot_insert(int32_t x, int32_t y, body_t * obj)
+void spatial_t::slot_insert(
+    int32_t x,
+    int32_t y,
+    body_t * obj)
 {
     std::list<slot_t> & list = slot(x, y);
     list.push_front(slot_t{x, y, obj});
 }
 
-void spatial_t::remove(body_t * obj)
+void spatial_t::remove(const body_t * obj)
 {
     bound_t ob = object_bound(obj);
     for (int32_t y = ob.y0; y<=ob.y1; ++y) {
@@ -136,8 +168,8 @@ void spatial_t::query_collisions(body_pair_set_t & out)
                 // pairs under test
                 auto oa = a->obj, ob = b->obj;
                 // distance between objects
-                const float dx = ob->x - oa->x;
-                const float dy = ob->y - oa->y;
+                const float dx = ob->p.x - oa->p.x;
+                const float dy = ob->p.y - oa->p.y;
                 const float ds = dx*dx + dy*dy;
 
                 // ideal distance
@@ -157,43 +189,26 @@ void spatial_t::query_collisions(body_pair_set_t & out)
 }
 
 void spatial_t::query_radius(
-    float x,
-    float y,
+    const vec2f_t & p,
     float r,
     body_set_t & out)
 {
 #if 0
     // ideal distance
     const float id = (obj->r + r) * (obj->r + r);
+#else
+    assert(!"not yet implemented");
 #endif
 }
 
-static bool overlap(
-    body_t * obj,
-    float x0,
-    float y0,
-    float x1,
-    float y1)
-{
-    assert(obj);
-    const float cx = clamp( x0, obj->x, x1 );
-    const float cy = clamp( y0, obj->y, y1 );
-    const float dx = obj->x - cx;
-    const float dy = obj->y - cy;
-    const float ds = dx*dx + dy*dy;
-    return ds < (obj->r * obj->r);
-}
-
 void spatial_t::query_rect(
-    float x0,
-    float y0,
-    float x1,
-    float y1,
+    const vec2f_t & p0,
+    const vec2f_t & p1,
     body_set_t & out)
 {
     // transform bounds into hash space
-    int32_t sx0 = x0, sy0 = y0;
-    int32_t sx1 = x1, sy1 = y1;
+    int32_t sx0 = p0.x, sy0 = p0.y;
+    int32_t sx1 = p1.x, sy1 = p1.y;
     xform_in(sx0, sy0);
     xform_in(sx1, sy1);
 
@@ -205,8 +220,7 @@ void spatial_t::query_rect(
 
             for (auto a = cell.cbegin(); a != cell.cend(); ++a) {
 
-                if (overlap(a->obj, x0, y0, x1, y1)) {
-
+                if (overlap(a->obj, p0.x, p0.y, p1.x, p1.y)) {
                     out.insert(a->obj);
                 }
             }
@@ -215,13 +229,11 @@ void spatial_t::query_rect(
 }
 
 void spatial_t::query_ray(
-    float x1,
-    float y1,
-    float x2,
-    float y2,
+    const vec2f_t & p0,
+    const vec2f_t & p1,
     body_set_t & out)
 {
-
+    assert(!"not yet implemented");
 }
 
 int spatial_t::dbg_ocupancy(int32_t x, int32_t y)
--- framework/spatial.h
+++ framework/spatial.h
@@ -5,10 +5,29 @@
 #include <cstring>
 #include <functional>
 
+#include "vec2.h"
+
 struct body_t
 {
-    float x, y;
-    float vx, vy;
+    body_t(const vec2f_t & pos, const float radius)
+        : p(pos)
+        , r(radius)
+    {
+    }
+
+    vec2f_t pos() const
+    {
+        return p;
+    }
+
+    float radius() const
+    {
+        return r;
+    }
+
+protected:
+    friend struct spatial_t;
+    vec2f_t p;
     float r;
 };
 
@@ -44,11 +63,13 @@ struct body_pair_set_t
 
     typedef std::unordered_set<pair_t, hash_func_t> base_t;
 
-    base_t::iterator begin() {
+    base_t::iterator begin()
+    {
         return base_.begin();
     }
 
-    base_t::iterator end() {
+    base_t::iterator end()
+    {
         return base_.end();
     }
 
@@ -66,11 +87,13 @@ struct body_set_t
 
     bool operator[] (body_t * a) const;
 
-    base_t::iterator begin() {
+    base_t::iterator begin()
+    {
         return base_.begin();
     }
 
-    base_t::iterator end() {
+    base_t::iterator end()
+    {
         return base_.end();
     }
 
@@ -80,6 +103,41 @@ protected:
 
 struct spatial_t
 {
+    void insert(body_t * obj);
+
+    void remove(const body_t * obj);
+
+    void move(body_t * obj, const vec2f_t & pos)
+    {
+        assert(obj);
+        bound_t ob0 = object_bound(obj);
+        obj->p = pos;
+        bound_t ob1 = object_bound(obj);
+        move(obj, ob0, ob1);
+    }
+
+    void move(body_t * obj, const vec2f_t & pos, const float radius)
+    {
+        assert(obj);
+        bound_t ob0 = object_bound(obj);
+        obj->p = pos;
+        obj->r = radius;
+        bound_t ob1 = object_bound(obj);
+        move(obj, ob0, ob1);
+    }
+
+    void query_collisions(body_pair_set_t & out);
+
+    void query_radius(const vec2f_t & p, float r, body_set_t & out);
+
+    void query_rect(const vec2f_t & p0, const vec2f_t & p1, body_set_t & out);
+
+    void query_ray(const vec2f_t & p0, const vec2f_t & p1, body_set_t & out);
+
+    int dbg_ocupancy(int32_t x, int32_t y);
+
+protected:
+
     static const uint32_t width = 32;
     static const uint32_t items = 1024;
 
@@ -109,39 +167,12 @@ struct spatial_t
 
     std::list<slot_t> & slot(int32_t x, int32_t y);
 
-    bound_t object_bound(body_t * obj);
+    bound_t object_bound(const body_t * obj) const;
 
-    void insert(body_t * obj);
-
-    void slot_erase(int32_t x, int32_t y, body_t * obj);
+    void slot_erase(int32_t x, int32_t y, const body_t * obj);
 
     void slot_insert(int32_t x, int32_t y, body_t * obj);
 
-    void remove(body_t * obj);
-
-    void move(body_t * obj, std::function<void(body_t&)> func)
-    {
-        // old object bound
-        bound_t ob0 = object_bound(obj);
-        // move object
-        func(*obj);
-        // new object bound
-        bound_t ob1 = object_bound(obj);
-        // update hash map
-        move(obj, ob0, ob1);
-    }
-
-    void query_collisions(body_pair_set_t & out);
-
-    void query_radius(float x, float y, float r, body_set_t & out);
-
-    void query_rect(float x0, float y0, float x1, float y1, body_set_t & out);
-
-    void query_ray(float x0, float y0, float x1, float y1, body_set_t & out);
-
-    int dbg_ocupancy(int32_t x, int32_t y);
-
-protected:
     void move(body_t *, const bound_t &, const bound_t &);
 
     void xform_in(int32_t & x, int32_t & y)
